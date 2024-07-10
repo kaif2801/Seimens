3591e591b1a5a9405231af6d46ef93d45a90310b
kaif2801 <kaifraza8329@gmail.com>
2024-07-10 16:16:12 +0530

    third commit

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform{
-    backend "s3"
-    {
-        bucket = "467.devops.candidate.exam"
-        Region: "ap-south-1"
-        Key: "<Your First Name>.<You Last Name>"
-    }
-}
--- provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-    region = "ap-south-1"
-    acess_key = "<your-access-key>"
-    secret_key = "<your-secret-key>"
-}
